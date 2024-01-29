c5230e2b00bcd852c569fc3c37be264f1bca465b
Tannu Agarwal <117573963+TannuAgarwal113322@users.noreply.github.com>
2024-01-30 02:13:58 +0530

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    int numIdenticalPairs(vector<int>& nums) 
+    vector<int> getConcatenation(vector<int>& nums) 
     {
-        vector<int>nums;
-        int count=0;
+        vector<int>ans;
         for(int i=0;i<nums.size();i++)
         {
-            for(int j=0;j<nums.size();j++)
-            {
-                if(i<j)
-                {
-                    if(a[i]==a[j])
-                    {
-                        count++;
-                    }
-                }
-            }
+            ans.push_back(nums[i]);
         }
-        return count;
-        
+        for(int i=0;i<nums.size();i++)
+        {
+            ans.push_back(nums[i]);
+        }
+        return ans;
     }
 };
